de8e16c9ea69812ac4f15256c2c887fd13ae11ee
Nelu_Dragan <dragannelu02@gmail.com>
2023-07-12 11:38:12 +0300

    created branch for new eveniment

--- src/components/organisms/Login.js
+++ src/components/organisms/Login.js
@@ -1,37 +1,36 @@
-"use client";
+'use client'
 
-import ButtonAtom from "../atoms/Button";
-import InputAtom from "../atoms/Input";
-import "bootstrap/dist/css/bootstrap.css";
-import { Navigate, useNavigate } from "react-router-dom";
+import ButtonAtom from '../atoms/Button'
+import InputAtom from '../atoms/Input'
+import 'bootstrap/dist/css/bootstrap.css'
+import { Navigate, useNavigate } from 'react-router-dom'
 
 export default function LoginForm() {
-  let navigate = useNavigate();
   const handleSubmit = async (event) => {
-    event.preventDefault();
+    event.preventDefault()
     const data = {
       email: event.target.email.value,
       password: event.target.password.value,
-    };
-    const JSONdata = JSON.stringify(data);
-    const endpoint = "http://localhost:3001/login";
+    }
+    const JSONdata = JSON.stringify(data)
+    const endpoint = 'http://localhost:3001/login'
     const options = {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSONdata,
-    };
-    const response = await fetch(endpoint, options);
-    const result = await response.json();
+    }
+    const response = await fetch(endpoint, options)
+    const result = await response.json()
     if (result.admin == true) {
-     // navigate("/admin");
-      console.log("admin");
+      // navigate("/admin");
+      console.log('admin')
     } else {
       // navigate("/user");
-      console.log("user");
+      console.log('user')
     }
-  };
+  }
   return (
     <div className="col-sm-7 bg-color align-self-center">
       <div className="form-section">
@@ -51,10 +50,10 @@ export default function LoginForm() {
             />
           </div>
           <div className="form-group">
-            <ButtonAtom label="Login" type={"submit"} />
+            <ButtonAtom label="Login" type={'submit'} />
           </div>
         </form>
       </div>
     </div>
-  );
+  )
 }
