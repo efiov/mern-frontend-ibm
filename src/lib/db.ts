27aafb2c43c45efa1f15d84a2ec461f65fe85bf5
Viorel Stefan <114824366+efiov@users.noreply.github.com>
2023-07-16 18:23:04 +0300

    remove useless files

--- src/lib/db.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-const url = process.env.DB_URL as string;
-let connection: typeof mongoose;
-const startDb = async () => {
-  if (!connection) connection = await mongoose.connect(url);
-  return connection;
-};
-
-export default startDb;
