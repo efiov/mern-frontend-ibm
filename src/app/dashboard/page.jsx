366facc6322c56af9df86b1d8562a4b1a6e0fb7f
Viorel Stefan <114824366+efiov@users.noreply.github.com>
2023-07-19 18:06:46 +0300

    style navbar

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -3,13 +3,16 @@
 import { useSession, signOut } from "next-auth/react";
 import Navbar from "../../components/molecules/Navbar";
 import Image from "next/image";
-import "../page.scss"
+import "../page.scss";
 export default function Home() {
   const { data: session, status, update } = useSession();
   return (
     <>
-      <Navbar user={session?.user}/>
-<h1>d</h1>
+      <Navbar user={session?.user} />
+      <div className="dashboard">
+        {/* Content  */}
+        
+        </div>
     </>
   );
 }
