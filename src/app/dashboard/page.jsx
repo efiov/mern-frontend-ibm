e65c2c92a629877a55701c8dc7bd4f88bbcea297
Viorel Stefan <114824366+efiov@users.noreply.github.com>
2023-07-19 17:38:09 +0300

    comp navbar dash

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,21 +1,15 @@
 "use client";
 
 import { useSession, signOut } from "next-auth/react";
+import Navbar from "../../components/molecules/Navbar";
 import Image from "next/image";
+import "../page.scss"
 export default function Home() {
   const { data: session, status, update } = useSession();
   return (
     <>
-      <h1>{session?.user.name}</h1>
-      <button
-        onClick={() =>
-          signOut({
-            callbackUrl: `${window.location.origin}`,
-          })
-        }
-      >
-        Sign out
-      </button>
+      <Navbar user={session?.user}/>
+<h1>d</h1>
     </>
   );
 }
