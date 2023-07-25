8c09cc5535bbf6b715c14a473fd7367b92f880a7
Viorel Stefan <114824366+efiov@users.noreply.github.com>
2023-07-25 14:12:36 +0300

    update conf

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,33 @@
 "use client";
 
-import React from "react";
 import "./page.scss";
 import "bootstrap/dist/css/bootstrap.css";
-import "./assets/bg_img.jpg";
-import Page from "../components/organisms/LoginPage";
+import { useSession } from "next-auth/react";
+import Navbar from "../components/molecules/Navbar";
+import { redirect } from "next/navigation";
+import Admin from "../components/organisms/admin";
+import EventsList from "../components/organisms/EventsList";
 
 export default function Home() {
-  return <main>
-    <Page />
-    
-  </main>;
+  const { data: session, status } = useSession({
+    required: true,
+    onUnauthenticated() {
+      redirect("/login");
+    },
+  });
+  if (status === "loading") return <h1 className="loading">Loading ...</h1>;
+  console.log(session.user);
+  return (
+    <main>
+      <Navbar user={session?.user} />
+
+      <div className="dashboard">
+        {session?.user.role === "ADMIN" ? (
+          <Admin />
+        ) : (
+          <EventsList email={session.user.email} />
+        )}
+      </div>
+    </main>
+  );
 }
