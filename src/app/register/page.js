3716978fd874b039aada5b29afff9c905d649998
Viorel Stefan <114824366+efiov@users.noreply.github.com>
2023-07-25 14:14:35 +0300

    update reg page

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -3,19 +3,19 @@
 import React from "react";
 import BaseRegister from "../../components/molecules/BaseRegister";
 import RegisterForm from "../../components/molecules/Register";
-import GroupsPage from "../../components/organisms/Groups";
 import "../page.scss";
 import "bootstrap/dist/css/bootstrap.css";
 import "../assets/bg_img.jpg";
 
 export default function Page() {
   return (
-    <div className="form">
-      <div className="container">
-        <div className="row register-box">
-          <GroupsPage/>
-          {/* <BaseRegister />
-          <RegisterForm /> */}
+    <div className="fullBack">
+      <div className="form">
+        <div className="container">
+          <div className="row register-box">
+            <BaseRegister />
+            <RegisterForm />
+          </div>
         </div>
       </div>
     </div>
