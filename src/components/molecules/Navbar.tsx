168c5cd459177a425063b3d05e5a37e0e350d3e2
Viorel Stefan <114824366+efiov@users.noreply.github.com>
2023-07-26 22:54:33 +0300

    update profile info

--- src/components/molecules/Navbar.tsx
+++ src/components/molecules/Navbar.tsx
@@ -3,14 +3,20 @@
 import { Fragment } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
-import { signOut } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 import { MenuItem } from "@mui/material";
 import { Dialog } from "@headlessui/react";
 import { useState } from "react";
+import { toast } from "react-hot-toast";
 
-export default function Navbar({ user }: { user: any }) {
+export default function Navbar() {
+  const { data: session, status, update } = useSession();
   let [isOpen, setIsOpen] = useState(false);
+  const email = (email) =>
+    /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email);
+  const [newName, setNewName] = useState(session?.user.name);
+  const [newEmail, setNewEmail] = useState(session?.user.email);
 
   function closeModal() {
     setIsOpen(false);
@@ -19,6 +25,15 @@ export default function Navbar({ user }: { user: any }) {
   function openModal() {
     setIsOpen(true);
   }
+
+  function handleUpdate() {
+    if (!email(newEmail) || newName === "") {
+      toast.error("Invalid!");
+    } else {
+      update({ name: newName, email: newEmail });
+    }
+  }
+
   return (
     <>
       <Disclosure as="nav" className="bg-stone-100 bs">
@@ -35,10 +50,10 @@ export default function Navbar({ user }: { user: any }) {
                         <span className="sr-only">Open user menu</span>
                         <Image
                           className="h-8 w-8 rounded-full"
-                          src={user?.image || "https://i.pravatar.cc/"}
+                          src={session?.user.image || "https://i.pravatar.cc/"}
                           height={32}
                           width={32}
-                          alt={`${user?.name || "placeholder"} avatar`}
+                          alt={`${session?.user.name || "placeholder"} avatar`}
                         />
                       </Menu.Button>
                     </div>
@@ -52,7 +67,7 @@ export default function Navbar({ user }: { user: any }) {
                       leaveTo="transform opacity-0 scale-95"
                     >
                       <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-stone-100 py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                        <MenuItem>{user.name}</MenuItem>
+                        <MenuItem>{session?.user.name}</MenuItem>
                         <MenuItem>
                           <hr />
                           <button onClick={openModal}>Settings</button>
@@ -93,18 +108,18 @@ export default function Navbar({ user }: { user: any }) {
                   <div className="flex-shrink-0">
                     <Image
                       className="h-8 w-8 rounded-full"
-                      src={user?.image || "https://i.pravatar.cc/"}
+                      src={session?.user.image || "https://i.pravatar.cc/"}
                       height={32}
                       width={32}
-                      alt={`${user.name} avatar`}
+                      alt={`${session?.user.name} avatar`}
                     />
                   </div>
                   <div className="ml-3">
                     <div className="text-base font-medium text-gray-800">
-                      {user.name}
+                      {session?.user.name}
                     </div>
                     <div className="text-sm font-medium text-gray-500">
-                      {user.email}
+                      {session?.user.email}
                     </div>
                   </div>
                 </div>
@@ -157,9 +172,19 @@ export default function Navbar({ user }: { user: any }) {
                   >
                     My account
                   </Dialog.Title>
-                  <div className="mt-2 bg-stone-100">
+                  <div className="flex items-center px-4">
+                    <div className="flex-shrink-0">
+                      <Image
+                        className="h-10 w-10 rounded-full"
+                        src={session?.user.image || "https://i.pravatar.cc/"}
+                        height={32}
+                        width={32}
+                        alt={`${session?.user.name} avatar`}
+                      />
+                    </div>
+                  </div>
+                  <div className="mt-4 bg-stone-100">
                     <div className="isolate bg-stone-100 px-6 lg:px-8">
-                      
                       <form action="#" method="POST" className="">
                         <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
                           <div className="sm:col-span-2">
@@ -167,15 +192,16 @@ export default function Navbar({ user }: { user: any }) {
                               htmlFor="name"
                               className="block text-lg font-bold leading-6 text-gray-900"
                             >
-                              Name
+                              Name:
                             </label>
                             <div className="mt-2.5">
                               <input
                                 type="text"
                                 name="name"
                                 id="name"
-                                autoComplete="given-name"
-                                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                value={newName}
+                                onChange={(e) => setNewName(e.target.value)}
+                                className="block font-bold w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                               />
                             </div>
                           </div>
@@ -185,69 +211,33 @@ export default function Navbar({ user }: { user: any }) {
                               htmlFor="email"
                               className="block text-lg font-bold leading-6 text-gray-900"
                             >
-                              Email
+                              Email:
                             </label>
                             <div className="mt-2.5">
                               <input
                                 type="email"
                                 name="email"
                                 id="email"
-                                autoComplete="email"
-                                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                value={newEmail}
+                                onChange={(e) => setNewEmail(e.target.value)}
+                                className="block w-full font-bold rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                               />
                             </div>
                           </div>
-
-                          {/* <div className="sm:col-span-2">
-                            <label
-                              htmlFor="name"
-                              className="block text-lg font-bold leading-6 text-gray-900"
-                            >
-                              Old Password
-                            </label>
-                            <div className="mt-2.5">
-                              <input
-                                type="text"
-                                name="name"
-                                id="name"
-                                autoComplete="given-name"
-                                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                              />
-                            </div>
-                          </div> */}
-
-                          {/* <div className="sm:col-span-2">
-                            <label
-                              htmlFor="name"
-                              className="block text-lg font-bold leading-6 text-gray-900"
-                            >
-                              New Password
-                            </label>
-                            <div className="mt-2.5">
-                              <input
-                                type="text"
-                                name="name"
-                                id="name"
-                                autoComplete="given-name"
-                                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                              />
-                            </div>
-                          </div> */}
                         </div>
 
                         <div className="mt-10">
                           <button
-                            type="submit" onClick={closeModal}
+                            type="submit"
+                            onClick={handleUpdate}
                             className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                           >
-                           Update
+                            Update
                           </button>
                         </div>
                       </form>
                     </div>
                   </div>
-
-       
                 </Dialog.Panel>
               </Transition.Child>
             </div>
