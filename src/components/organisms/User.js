bd9947e3761b3c0095643e9b9e2a958fc13ac23b
Viorel Stefan <114824366+efiov@users.noreply.github.com>
2023-07-26 15:20:39 +0300

    user table

--- src/components/organisms/User.js
+++ src/components/organisms/User.js
@@ -1,48 +1,82 @@
 "use client";
-import React, { useState, useEffect } from "react";
-import Image from "next/image";
+
+import { Button } from "@mui/material";
+import { useState, useEffect } from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import axios from "axios";
 
 export default function User() {
   const [users, setUsers] = useState([]);
+
   useEffect(() => {
     const fetchPeople = async () => {
       try {
         const response = await fetch("http://localhost:3001/getUsers");
         const data = await response.json();
-        console.log(data);
         setUsers(data);
       } catch (error) {
         console.error("Error fetching people:", error);
       }
     };
-
     fetchPeople();
   }, []);
 
+  const updateRoleToAdmin = (id) => {
+    axios.post("http://localhost:3001/updateRoleToAdmin", {
+      id: id,
+    });
+  };
+
+  const updateRoleToUser = (id) => {
+    axios.post("http://localhost:3001/updateRoleToUser", {
+      id: id,
+    });
+  };
+
   return (
-    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-      <div className="grid gap-10 mx-auto lg:max-w-screen-lg sm:grid-cols-2 lg:grid-cols-4">
-        
-       {users.map((user) => (
-        <div key={user._id} className="flex flex-col items-center">
-          <Image
-            className="object-cover w-20 h-20 mb-2 rounded-full shadow"
-           // src={user?.}
-            alt="Person"
-            width={50} height={50}
-          />
-          <div className="flex flex-col items-center">
-            <p className="text-lg font-bold">{user.name}</p>
-            <p className="text-sm text-gray-800">{user.email}</p>
-          </div>
-        </div>
-       ))}
-
-        
-
-
-
-      </div>
-    </div>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }}>
+        <TableHead>
+          <TableRow>
+            <TableCell>Name</TableCell>
+            <TableCell>Email</TableCell>
+            <TableCell>Old Role</TableCell>
+            <TableCell>New role</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {users.map((user) => (
+            <TableRow key={user._id}>
+              <TableCell>{user.name}</TableCell>
+              <TableCell>{user.email}</TableCell>
+              <TableCell>{user.role}</TableCell>
+              <TableCell>
+                {user.role === "ADMIN" ? (
+                  <Button
+                    variant="outlined"
+                    onClick={() => updateRoleToUser(user._id)}
+                  >
+                    USER
+                  </Button>
+                ) : (
+                  <Button
+                    variant="outlined"
+                    onClick={() => updateRoleToAdmin(user._id)}
+                  >
+                    ADMIN
+                  </Button>
+                )}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
